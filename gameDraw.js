0477a22b3193caadfa71d383d26146d61bb873c4
Trevor P <tparsons008@gmail.com>
2019-10-16 10:05:46 -0500

    Deletes notes
    - Formatting done as well

--- gameDraw.js
+++ gameDraw.js
@@ -8,34 +8,3 @@ function drawBackground(){
     ctx.fillRect(0,0,570,220);
 
 }
-/*
-function playerSprite(){
-    this.type = type;
-    if (type == "image") {
-        this.image = new Image();
-        this.image.src = color;
-    }
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;    
-    this.x = x;
-    this.y = y;    
-    this.update = function() {
-        ctx = myGameArea.context;
-        if (type == "image") {
-            ctx.drawImage(this.image, 
-                this.x, 
-                this.y,
-                this.width, this.height);
-        } else {
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
-    }
-    this.newPos = function() {
-        this.x += this.speedX;
-        this.y += this.speedY;        
-    }
-    
-}*/
