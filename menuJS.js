652c5277c387990050032d2bd9bdb8565a3dd9f7
jwrice1188 <jwrice1188@gmail.com>
2019-10-16 11:46:05 -0500

    Adds Menu and functionality for "How To" button

---
+++ menuJS.js
@@ -0,0 +1,11 @@
+function start(){
+    //enter startup code here
+};
+
+function howto(){
+    alert("Use arrow keys to move character.\nAvoid moving and stationary obstacles.\nCollect coins as you progress.\nReach the end of the level before your time runs out!");
+};
+
+function exit(){
+    //enter exit code here
+};
