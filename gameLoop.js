f965ab39e215981b0e2ebc493dcda0daffcc71d2
tparsons008 <tparsons008@gmail.com>
2019-10-18 14:32:10 -0500

    Adds collectables
    - Coins are added that can be picked up
    - Introduces score
    - Introduces coins obtained

--- gameLoop.js
+++ gameLoop.js
@@ -10,17 +10,18 @@ var gameMaster = {
     score: 0,
     time: 600,
     lives: 3,
+    coins: 0,
     gameOn: true
 }
 
-var timer = setInterval(function(){   
+var timer = setInterval(function () {
     gameMaster.time = gameMaster.time - timer;
-    console.log(gameMaster.time);  
-},1000);
+    console.log(gameMaster.time);
+}, 1000);
 
 //gameObjects
-var obstacles = {};
-var collectables = {};
+var obstacles = [];
+var collectables = [];
 
 var img = new Image();
 img.src = 'sprites/frog.png';
@@ -28,8 +29,8 @@ var player = {
     sprite: img,
     sx: 0,
     sy: 0,
-    srcW:500, 
-    srcH:384,
+    srcW: 500,
+    srcH: 384,
     x: 200,
     y: 500,
     width: 20,
@@ -40,25 +41,26 @@ var player = {
 //gameObject calls
 gameObject(obstacles, "obstacle", "sprites/obstacle.png", 0, 0, 500, 385, 200, 200, 1, 20, 20, "obstacle1");
 gameObject(obstacles, "obstacle", "sprites/obstacle.png", 0, 0, 500, 385, 300, 100, 1, 20, 20, "obstacle2");
-gameObject(collectables, "collectable", "sprites/collectable.png",0, 0, 200, 200, 300, 300, 1, 20, 20, "collectable");
+gameObject(collectables, "collectable", "sprites/collectable.png", 0, 0, 200, 200, 300, 300, 1, 20, 20, "collectable1");
+gameObject(collectables, "collectable", "sprites/collectable.png", 0, 0, 200, 200, 215, 400, 1, 20, 20, "collectable2");
 
 // Game Logic Updates
 function update() {
     ctx.clearRect(0, 0, gameWindow.width, gameWindow.height);
     updateEntity(player);
     for (var key in obstacles) {
-        updateEntity(obstacles[key]); 
+        updateEntity(obstacles[key]);
     }
-    for (var key in collectables){
+    for (var key in collectables) {
         updateEntity(collectables[key]);
     }
 
-    if (gameMaster.lives == 0 || gameMaster.time == 0){
+    if (gameMaster.lives == 0 || gameMaster.time == 0) {
         cancelAnimationFrame(update);
         clearInterval(timer);
         console.log("Game Over");
-    } else{
-       requestAnimationFrame(update); 
+    } else {
+        requestAnimationFrame(update);
     }
 }
 
@@ -68,10 +70,13 @@ function updateEntity(entity) {
     ctx.drawImage(entity.sprite, entity.sx, entity.sy, entity.srcW, entity.srcH, entity.x, entity.y, entity.width, entity.height);
     controlPlayer();
     for (var key in obstacles) {
-    obstacleMove(obstacles[key]);
-    collideWith(obstacles[key]);
+        obstacleMove(obstacles[key]);
+        collideWith(obstacles[key]);
     }
-    
+    for (var key in collectables) {
+        collideWith(collectables[key]);
+    }
+
 }
 
 //Render Objects
@@ -91,9 +96,9 @@ function gameObject(gameObjectArray, gameObject, img, sx, sy, srcW, srcH, x, y, 
         spd: spd,
         id: id
     }
-    gameObjectArray[id] = gameObject;
+    gameObjectArray.push(gameObject);
 }
-
+console.log(obstacles.length);
 //player controller
 var upPress = false;
 var downPress = false;
@@ -106,19 +111,19 @@ function keyDownHandler(e) {
 
     if (e.keyCode == 39) {
         rightPress = true;
-        console.log("Right.");
+
     }
     if (e.keyCode == 37) {
         leftPress = true;
-        console.log("Left.");
+
     }
     if (e.keyCode == 38) {
         upPress = true;
-        console.log("Up.");
+
     }
     if (e.keyCode == 40) {
         downPress = true;
-        console.log("Down.");
+
     }
 }
 
@@ -133,39 +138,47 @@ function obstacleMove(obstacle) {
 
 function controlPlayer() {
 
-        if (upPress == true && player.y > 10) {
-            player.y = player.y - player.spd;
-            upPress = false;
-        } else if (downPress && player.y < 510) {
-            player.y = player.y + player.spd;
-            downPress = false;
-        } else if (leftPress && player.x > 10) {
-            player.x = player.x - player.spd;
-            leftPress = false;
-        } else if (rightPress && player.x < 540) {
-            player.x = player.x + player.spd;
-            rightPress = false;
-        }
+    if (upPress == true && player.y > 10) {
+        player.y = player.y - player.spd;
+        upPress = false;
+    } else if (downPress && player.y < 510) {
+        player.y = player.y + player.spd;
+        downPress = false;
+    } else if (leftPress && player.x > 10) {
+        player.x = player.x - player.spd;
+        leftPress = false;
+    } else if (rightPress && player.x < 540) {
+        player.x = player.x + player.spd;
+        rightPress = false;
+    }
 }
 
 //Colliders
 function collideWith(object) {
     if (player.x <= object.x + object.width / 2 && player.x >= object.x - object.width / 2 && player.y <= object.y + object.height / 2 && player.y >= object.y - object.height / 2) {
-        gameMaster.lives -= 1;
-        player.x = 200;
-        player.y = 500;
-        console.log(gameMaster.lives);
+        if (object.id.includes("obstacle")) {
+            gameMaster.lives -= 1;
+            player.x = 200;
+            player.y = 500;
+            console.log(gameMaster.lives);
+        } else if (object.id.includes("collectable")) {
+            console.log(collectables.indexOf(object));
+            collectables.splice(collectables.indexOf(object),1);
+            gameMaster.coins += 1;
+            gameMaster.score += 1;
+            console.log("Player score is: " + gameMaster.score + "\nCoins collected: " + gameMaster.coins);
+        }
     }
 }
 
 //Draw level
-function drawBackground(){
+function drawBackground() {
     ctx.fillStyle = "lime";
     ctx.fillRect(0, 440, 570, 45);
     ctx.fillRect(0, 220, 570, 45);
 
     ctx.fillStyle = "blue";
-    ctx.fillRect(0,0,570,220);
+    ctx.fillRect(0, 0, 570, 220);
 
 }
 
